0caed41bf60176580d57d393ae8a633dedbb6929
grigoryaanA <117577838+grigoryaanA@users.noreply.github.com>
2023-01-26 18:59:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,125 +1,115 @@
-from carmarket import CarMarket
+from store import CarMarket
+from car import Car
 
 market = CarMarket()
+date = {
+    "year": 2022,
+    "moth": 1,
+    "day": 1,
+}
 
 
+def update_date():
+    if date["day"] <= 30:
+        date["day"] += 1
+    elif date["day"] == 30:
+        date["day"] = 1
+        if date["moth"] < 12:
+            date["moth"] += 1
+        else:
+            date["moth"] = 1
+            date["year"] += 1
+
+
+market.load_data()
 while True:
-    print("CarMarket:")
-    print("To add seller enter Seller: ")
-    print("To add buyer enter Buyer: ")
-    print("To add car enter Car: ")
-    print("To sell car enter S: ")
-    print("To return car enter R: ")
-    print("To get seller available cars SC:")
-    print("To get market history enter H: ")
-    print("To get cars available discount: CD")
-    print("To set discount for a car: SD")
-    print("To bind car to a seller: B\n\n")
+    market.get_data()
+    # display menu options
+    print("1. Add a car to the market")
+    print("2. Add a buyer to the market")
+    print("3. Add a seller to the market")
+    print("4. Buy a car")
+    print("5. Return a car")
+    print("6. Get car discount")
+    print("7. Get transactions")
+    print("8. Exit\n")
 
-    choose = input("Enter your option: ")
+    choose = int(input("Enter option: "))
 
-    if choose == "Seller":
-        name = input("Enter sellers name: ")
-        surname = input("Enter sellers surname: ")
-        city = input("Enter sellers city: ")
-        money = int(input("Enter sellers money: "))
-        market.add_seller(name, surname, city, money)
-
-    if choose == "Buyer":
-        name = input("Enter buyers name: ")
-        surname = input("Enter buyers surname: ")
-        city = input("Enter buyers city: ")
-        money = int(input("Enter buyer money: "))
-        market.add_buyer(name, surname, city, money)
-
-    if choose == "Car":
-        model = input("Enters cars model: ")
-        uid = int(input("Enter cars id: "))
-        price = int((input("Enter cars price: ")))
-        market.add_car(model, uid, price)
-
-    if choose == "S":
-        seller_n = input("Enter seller name: ")
-        buyer_n = input("Enter buyer name: ")
-        car_id = input("Enter car id: ")
-        sel_car , buy, sel = None, None, None
-        for seller in market.sellers:
-            if seller_n == seller.name:
-                sel = seller
-            else:
-                print("No seller found! ")
-        for buyer in market.buyers:
-            if buyer_n == buyer.name:
-                buy = buyer
-            else:
-                print("No buyer found! ")
-        for car in market.cars:
-            if car_id == car.id:
-                sel_car = car
-            else:
-                print("No car found! ")
-
-        market.sell(sel_car, sel, buy)
-
-    if choose == "R":
-        seller_n = input("Enter seller name: ")
-        buyer_n = input("Enter buyer name: ")
-        car_id = input("Enter car id: ")
-        sel_car, buy, sel = None, None, None
-        for seller in market.sellers:
-            if seller_n == seller.name:
-                sel = seller
-            else:
-                print("No seller found! ")
-        for buyer in market.buyers:
-            if buyer_n == buyer.name:
-                buy = buyer
-            else:
-                print("No buyer found! ")
-        for car in market.cars:
-            if car_id == car.id:
-                sel_car = car
-            else:
-                print("No car found! ")
-
-        market.return_car(sel_car, buy, sel)
-
-    if choose == "SC":
-        name = input("Enter sellers name: ")
-        market.get_seller_available_cars(name)
-
-    if choose == "H":
-        market.get_history()
-
-    if choose == "CD":
-        car_id = int(input("Enter cars id: "))
-        check_car = None
-        for car in market.cars:
-            if car_id == car.id:
-                check_car = car
-        market.get_car_available_discount(check_car)
-
-    if choose == "SD":
+    if choose == 1:
+        model = input("Enter model: ")
+        uid = int(input("Enter car id: "))
+        price = int(input("Enter car price: "))
         discount = int(input("Enter discount: "))
-        car_id = int(input("Enter cars id: "))
-        check_car = None
-        for car in market.cars:
-            if car_id == car.id:
-                check_car = car
-        market.set_discount(check_car,discount)
+        seller_name = input("Enter seller name: ")
 
-    if choose == "B":
-        seller_n = input("Enter seller name: ")
-        car_id = input("Enter car id: ")
-        sel_car, sel = None, None
         for seller in market.sellers:
-            if seller_n == seller.name:
-                sel = seller
-            else:
-                print("No seller found! ")
-        for car in market.cars:
-            if car_id == car.id:
-                sel_car = car
-            else:
-                print("No car found! ")
-        market.bind_car_to_seller(sel_car, sel)
+            if seller.name == seller_name:
+                car = Car(model, uid, price, seller, discount)
+                market.add_car(car)
+                print(seller.car_park)
+
+    if choose == 2:
+        loc_name = input("Enter buyer name: ")
+        loc_surname = input("Enter buyer surname: ")
+        loc_city = input("Enter buyer city: ")
+        loc_money = int(input("Enter buyer money: "))
+        market.add_buyer(loc_name, loc_surname, loc_city, loc_money)
+
+    if choose == 3:
+        loc_name = input("Enter seller name: ")
+        loc_surname = input("Enter seller surname: ")
+        loc_city = input("Enter seller city: ")
+        loc_money = int(input("Enter seller money: "))
+        market.add_seller(loc_name, loc_surname, loc_city, loc_money)
+
+    if choose == 4:
+        car_id = int(input("Enter car's id: "))
+        buyer_name = input("Enter buyers name: ")
+        buyer = None
+        car = None
+
+        for buyer_loc in market.buyers:
+            if buyer_loc.name == buyer_name:
+                buyer = buyer_loc
+                break
+        if buyer is None:
+            print("No such buyer! ")
+
+        for car_loc in market.car_park:
+            if car_loc.id == car_id:
+                car = car_loc
+                break
+        if car is None:
+            print("No such car! ")
+
+        if buyer is not None and car is not None:
+            market.sell(car, buyer, date)
+
+    if choose == 5:
+        car_id = int(input("Enter car's id: "))
+        car = None
+        for car_loc in market.car_park:
+            if car_loc.id == car_id:
+                car = car_loc
+                break;
+        if car is None:
+            print("No such car!")
+        market.return_car(car, date)
+
+    if choose == 6:
+        car_id = int(input("Enter car's id: "))
+        car = None
+        for car_loc in market.car_park:
+            if car_loc.id == car_id:
+                car = car_loc
+        market.get_car_available_discount(car)
+
+    if choose == 7:
+        for transaction in market.transactions:
+            print(transaction)
+
+    if choose == 8:
+        break
+
+    update_date()
